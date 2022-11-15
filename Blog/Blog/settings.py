376a11ec3a6b6b4eb295bae856a7bb3f88c05fd6
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 15:15:50 +0200

    media files configuration

--- Blog/Blog/settings.py
+++ Blog/Blog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,3 +139,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+# # BASE_DIR----> 127.0.0.1/media  --> absolute path
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 # from main import urls
@@ -7,4 +9,4 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include('django.contrib.auth.urls')),
     path('category/', include('category.urls'))
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
