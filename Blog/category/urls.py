3524a5de8179ad72a33c8835046084fb9d1599c6
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 12:37:31 +0200

    created category index view

--- Blog/category/urls.py
+++ Blog/category/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('create', views.category_create),
+    path('', views.category_index),
+    path('index', views.category_index),
 ]
--- Blog/category/views.py
+++ Blog/category/views.py
@@ -1,7 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import CategoryCreateForm
+from .models import CategoryModel
+
+
+def _category_create_post(request):
+    form = CategoryCreateForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('/category')
+    return render(request, 'category/create.html', {'form': form})
 
 
 def category_create(request):
+    if request.POST:
+        return _category_create_post(request)
+    
     form = CategoryCreateForm()
     return render(request, 'category/create.html', {'form': form})
+
+
+def category_index(request):
+    categories = CategoryModel.objects.all()
+    return render(request, 'category/index.html', {'categories': categories})
