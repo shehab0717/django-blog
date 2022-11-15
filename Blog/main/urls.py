6befb9a7454aa23b258cf1f2edeff6d2954b17ad
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 11:02:18 +0200

    create home view

--- Blog/main/urls.py
+++ Blog/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sign-up', views.RegisterView)
+    path('sign-up', views.register_view, name='sign_up'),
+    path('', views.home_view, name='home'),
 ]
--- Blog/main/views.py
+++ Blog/main/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
-# Create your views here.
+def _register_post(request):
+    form = RegisterForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect('')
 
-def RegisterView(request):
+def _register_get(request):
     form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
 
-# def Index(request):
-#     return render(request, 'regi')
+def register_view(request):
+    if request.method == 'POST':
+        return _register_post(request)
+    return _register_get(request)
+
+
+
+
+def home_view(request):
+    return render(request, 'home.html')
