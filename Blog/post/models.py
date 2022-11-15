c41d393f537819da5a526d74f655a76dcab01527
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 15:23:16 +0200

    update models (tag, posts)

--- Blog/post/models.py
+++ Blog/post/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from category.models import Category
 
+class Tag(models.Model):
+    name=models.CharField(max_length=20, primary_key=True)
+
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     author_id = models.ForeignKey(User, on_delete= models.CASCADE)
     created_at = models.TimeField(auto_now_add=True)
@@ -17,9 +21,6 @@ class PostReaction(models.Model):
     post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
 
-class Tag(models.Model):
-    name=models.CharField(max_length=20, primary_key=True)
-    posts = models.ManyToManyField(Post)
 
 class Comment(models.Model):
     text = models.TextField()
