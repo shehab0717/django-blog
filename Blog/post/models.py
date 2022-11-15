db2930982386a43f339e2df9ea24a98c5dac3ddb
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 14:48:23 +0200

    create models (post, tag, post_reaction)

--- Blog/post/models.py
+++ Blog/post/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    author_id = models.ForeignKey(User, on_delete= models.CASCADE)
+
+
+class PostReaction(models.Model):
+    liked = models.BooleanField()
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Tag(models.Model):
+    name=models.CharField(max_length=20, primary_key=True)
+    posts = models.ManyToManyField(Post)
