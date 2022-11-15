b9e8a7c34f9987e9e6faeb2f6c84c82de8f1876f
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 11:20:52 +0200

    category model

--- Blog/category/models.py
+++ Blog/category/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class CategoryModel(models.Model):
+    name = models.CharField(max_length=50)
+    created_at = models.TimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
