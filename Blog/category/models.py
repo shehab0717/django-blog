c8b18adb0bb99f1187cacb8ce546f46907f695e6
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 16:02:33 +0200

    update __str__ method of category

--- Blog/category/models.py
+++ Blog/category/models.py
@@ -6,3 +6,6 @@ class Category(models.Model):
     name = models.CharField(max_length=50)
     created_at = models.TimeField(auto_now_add=True)
     updated_at = models.TimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
