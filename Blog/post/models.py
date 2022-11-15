94c60524fe13fb8eaecbedd8eb0e752d4df0d81a
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-15 15:16:52 +0200

    add comment model and update post model fields (added image field, category, and timestamps)

--- Blog/post/models.py
+++ Blog/post/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from category.models import Category
 
 class Post(models.Model):
-
     title = models.CharField(max_length=100)
     content = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     author_id = models.ForeignKey(User, on_delete= models.CASCADE)
+    created_at = models.TimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
 
 
 class PostReaction(models.Model):
@@ -16,3 +20,11 @@ class PostReaction(models.Model):
 class Tag(models.Model):
     name=models.CharField(max_length=20, primary_key=True)
     posts = models.ManyToManyField(Post)
+
+class Comment(models.Model):
+    text = models.TextField()
+    is_reply = models.BooleanField(default=False)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    parent_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.TimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
