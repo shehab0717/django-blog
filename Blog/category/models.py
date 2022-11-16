b0d84da0ed0f160931013ca30d7f2309d218519b
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-16 13:19:16 +0200

    subscribtion relation between user and category

--- Blog/category/models.py
+++ Blog/category/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=50)
+    subscribers = models.ManyToManyField(User)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
