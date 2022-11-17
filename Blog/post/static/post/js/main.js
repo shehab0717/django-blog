8f38a96271d250c5120e220ac164b319603a5d15
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-17 13:58:05 +0200

    add form (create tag) to the post create template

---
+++ Blog/post/static/post/js/main.js
@@ -0,0 +1,22 @@
+tagForm = document.getElementById('tag-form')
+
+tagForm.onsubmit = form_submit
+
+
+function form_submit(event){
+    form = event.target
+    tagName = form[1].value
+    csrf_token = form[0].value
+    console.log(tagName)
+    $.ajax({
+        type: "POST",
+        url: '/tag/create',
+        data: {
+            name: tagName,
+            csrfmiddlewaretoken: csrf_token
+        },
+        success: ()=> $('#tagFormModal').modal('toggle')
+      });
+    return false
+}
+
