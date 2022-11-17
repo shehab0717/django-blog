d630b683f886a1f4ac197b54ecae1371b9ad9fbc
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-17 13:56:54 +0200

    tag CRUD

--- Blog/tag/urls.py
+++ Blog/tag/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('index', views.tag_index, name='tag_index'),
+    path('create', views.tag_create, name='tag_create'),
+    path('update/<str:name>', views.tag_update, name='tag_update'),
+    path('delete/<str:name>', views.tag_delete, name='tag_delete'),
 ]
--- Blog/tag/views.py
+++ Blog/tag/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tag
+from .forms import CreateTagForm
+from django.core.exceptions import ObjectDoesNotExist
 
-# Create your views here.
+
+def tag_create(request):
+    form = CreateTagForm()
+    if request.POST:
+        form = CreateTagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    return render(request, 'tag/create.html', {'form': form})
+
+
+def tag_index(request):
+    tags = Tag.objects.all()
+    return render(request, 'tag/index.html', {'tags': tags})
+
+
+def tag_update(request, name):
+    tag = Tag.objects.filter(name = name)
+    if not tag:
+        raise ObjectDoesNotExist()
+    tag = tag.first()
+    form = CreateTagForm()
+    if request.POST:
+        form = CreateTagForm( request.POST, instance=tag)
+        if form.is_valid():
+            form.save()
+            return redirect('/tag/index')
+    return render(request, 'tag/update.html', {'form', form})
+
+def tag_delete(request, name):
+    tag = Tag.objects.filter(name = name)
+    if not tag:
+        raise ObjectDoesNotExist()
+    tag = tag.first()
+    tag.delete()
+    return redirect('/tag/index')
