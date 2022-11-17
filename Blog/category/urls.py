2edc84e3219b8eeed4eb4e8d029a1d785217287a
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-17 14:16:49 +0200

    assign names for category urls

--- Blog/category/urls.py
+++ Blog/category/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.category_index),
+    path('', views.category_index, name='categor_index'),
     path('index', views.category_index),
-    path('create', views.category_create),
-    path('update/<int:id>', views.category_update),
-    path('delete/<int:id>', views.category_delete),
+    path('create', views.category_create, name='categor_create'),
+    path('update/<int:id>', views.category_update, name='categor_update'),
+    path('delete/<int:id>', views.category_delete, name='categor_delete'),
     path('details/<int:id>', views.category_details, name='category_details'),
     path('subscribe/<int:id>', views.category_toggle_subscribe, name='category_toggle_subscribe')
 ]
