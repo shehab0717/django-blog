84fd882824c00dd153b204a83494e3b99360b591
Shehab Abdullah <shehab.abdullah12@gmail.com>
2022-11-21 15:38:11 +0200

    authorize admin for accessing badword pages

--- Blog/badword/views.py
+++ Blog/badword/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import BadWord
+from cadmin.decorators import staff_member_required
 
 # Create your views here.
 
+@staff_member_required(login_url='/login')
 def badword_index(request):
     words = BadWord.objects.all()
     return render(request, 'badword/index.html', {'words': words})
 
+@staff_member_required(login_url='/login')
 def badword_create(request):
     if request.method == 'POST':
         badword = BadWord(text = request.POST['text'])
@@ -15,6 +18,7 @@ def badword_create(request):
         return redirect(reverse('badword_index'))
     return render(request, 'shared/not_found.html')
 
+@staff_member_required(login_url='/login')
 def badword_delete(request, id):
     if request.POST:
         word = BadWord.objects.get(pk = id)
